416365ffd8ec3a194f23f25e9c9dd88c5d03b059
Jovanni Feliz <feliz.jovanni@gmail.com>
2025-09-10 21:53:02 -0500

    Added function declaration to python-start file

--- python-start.py
+++ python-start.py
@@ -28,3 +28,107 @@ vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
 dirty_dozen = [fruits, vegetables]
 
 print(dirty_dozen[0][2])
+
+# def greet(intro, x):
+#     """Prints a greeting with an introduction and a name."""
+#     print(f"{intro}")
+#     print("I\'m")
+#     print(x)
+
+# greet("Welcome","Ashley")
+
+
+def life_in_weeks(age):
+    print(f"Your current age is: {age}")
+    # 52 weeks in 1 year
+    weeks_in_age = age * 52
+    weeks_left = 4680 - weeks_in_age
+    print(
+        f"""You have {weeks_left // 52} years left until 90. Make them count!\n
+          You have {weeks_left} weeks left.
+          """
+    )
+
+
+life_in_weeks(39)
+
+
+def life_in_weeks2(age=20):
+    print(f"Your current age is: {age}")
+    # 52 weeks in 1 year
+    weeks_in_age = age * 52
+    weeks_left = 4680 - weeks_in_age
+    print(
+        f"""You have {weeks_left // 52} years left until 90. Make them count!\n
+          You have {weeks_left} weeks left.
+          """
+    )
+
+
+life_in_weeks2(80)
+
+
+def calculate_love_score(name1="Angela Yu", name2="Jack Bauer"):
+    arr = []
+    it_true = 0
+    it_love = 0
+    
+    name_1st = name1.lower()
+    name_2nd = name2.lower()
+    # store each name into an array
+    for x in name_1st:
+        arr += x
+
+    for y in name_2nd:
+        arr += y
+
+    t_count = 0
+    r_count = 0
+    u_count = 0
+    e_count = 0
+    
+    l_count = 0
+    o_count = 0
+    v_count = 0
+
+    for letter in arr:
+        if letter == "t":
+            t_count = arr.count(letter)
+        elif letter == "r":
+            r_count = arr.count(letter)
+        elif letter == "u":
+            u_count = arr.count(letter)
+        elif letter == "e":
+            e_count = arr.count(letter)
+        elif letter == "l":
+            l_count = arr.count(letter)
+        elif letter == "o":
+            o_count = arr.count(letter)
+        elif letter == "v":
+            v_count = arr.count(letter)
+        else:
+            continue
+        
+        it_true = t_count + r_count + u_count + e_count
+        it_love = l_count + o_count + v_count + e_count
+        
+        true_out = str(it_true)
+        love_out = str(it_love)
+
+    print(f"""
+            T: {t_count}
+            R: {r_count}
+            U: {u_count}
+            E: {e_count}
+            total: {true_out}
+            
+            L: {l_count}
+            O: {o_count}
+            V: {v_count}
+            E: {e_count}
+            total: {love_out}
+            
+            Love Score = {int(true_out + love_out)}
+            """)
+
+calculate_love_score()
